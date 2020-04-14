e4bfbe1c5137e835f9335fae4dd78d1e66112a63
Pam <pamela@gluss.com>
2020-04-14 14:34:14 -0700

    first run at travis integration

--- run.sh
+++ run.sh
@@ -1,17 +1,43 @@
-## Using FTP with Bash is... not recommended but let's see if we can do it
+#!/bin/sh
 
+echo "*** Install Linuxbrew ***"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+
+echo '** Installing inetutils which gives us access to FTP**'
 brew install inetutils
 
 HOST=$GODADDY_HOST
 USER=$GODADDY_USER
 PASSWD=$GODADDY_PASSWORD
-FILE='file.txt'
 
-ftp -n $HOST <<END_SCRIPT
+echo '** Ftp to Godaddy... **'
+ftp -pinv $HOST <<END_SCRIPT
 quote USER $USER
 quote PASS $PASSWD
-binary
-put $FILE
-quit
+hash
+
+mput *.html
+mput *.md
+
+cd css/
+lcd css/
+mput *.css
+
+cd ../js/
+lcd ../js/
+mput *.js
+
+cd ../img/
+lcd ../img/
+mput *.*
+
+bye
 END_SCRIPT
 exit 0
+
+echo '** Upload finished, build complete **'
